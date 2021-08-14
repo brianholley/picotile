6cfcb4d9d5fb35b4a10855e69069dc9deeac2af0
Brian Holley <brian.holley@hotmail.com>
2021-08-14 15:33:48 -0700

    Hook up web app to test service backend (#2)

--- data/src/PicotileApp.jsx
+++ data/src/PicotileApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -11,18 +11,63 @@ import Menu from './Menu'
 import Settings from './Settings'
 import TileField from './TileField'
 
-const tiles = [
-    {type: 'control', pos: {x: 4, y: 5, z: 1}}, 
-    {type: 'light', index: 0, pos: {x: 4, y: 4, z: 0}, color: '#b0b'},
-    {type: 'light', index: 0, pos: {x: 3, y: 4, z: 1}, color: '#4B0082'},
-    {type: 'light', index: 0, pos: {x: 3, y: 3, z: 0}, color: '#00c'},
-    {type: 'light', index: 0, pos: {x: 3, y: 3, z: 1}, color: '#0d0'},
-    {type: 'light', index: 0, pos: {x: 3, y: 2, z: 0}, color: '#dd0'},
-    {type: 'light', index: 0, pos: {x: 2, y: 2, z: 1}, color: '#d70'},
-    {type: 'light', index: 0, pos: {x: 2, y: 2, z: 0}, color: '#c00'},
-  ]
+import * as Api from './api'
+
+const defaultTiles = {
+    version: 1,
+    maxTiles: 20,
+    tiles: [
+        {type: 'control', pos: {x: 4, y: 5, z: 1}}, 
+        {type: 'light', index: 0, pos: {x: 4, y: 4, z: 0}, color: '#b0b'},
+        {type: 'light', index: 0, pos: {x: 3, y: 4, z: 1}, color: '#4B0082'},
+        {type: 'light', index: 0, pos: {x: 3, y: 3, z: 0}, color: '#00c'},
+        {type: 'light', index: 0, pos: {x: 3, y: 3, z: 1}, color: '#0d0'},
+        {type: 'light', index: 0, pos: {x: 3, y: 2, z: 0}, color: '#dd0'},
+        {type: 'light', index: 0, pos: {x: 2, y: 2, z: 1}, color: '#d70'},
+        {type: 'light', index: 0, pos: {x: 2, y: 2, z: 0}, color: '#c00'},
+    ]
+}
+
+const defaultSettings = {
+    brightness: 255,
+    speed: 0,
+    mode: 0
+}
 
 let PicotileApp = props => {
+
+    const [loaded, setLoaded] = useState(false)
+    const [tiles, setTiles] = useState(defaultTiles)
+    const [settings, setSettings] = useState(defaultSettings)
+
+    useEffect(() => {
+        let loadAsync = async () => {
+            if (!loaded)
+            {
+                let tileQuery = await Api.getTiles();
+                setTiles(tileQuery)
+
+                let settingsQuery = await Api.getSettings()
+                setSettings(settingsQuery)
+
+                setLoaded(true)
+            }
+        }
+        loadAsync()
+    }, [tiles, settings])
+
+    let onChangeSetting = async (newSettings) => {
+        let oldSettings = settings
+        setSettings(newSettings)
+        try {
+            await Api.postSettings(newSettings)
+        }
+        catch {
+            console.log("Error")
+            setSettings(oldSettings)
+        }
+    }
+
     return (
         <Router>
             <div className="App">
@@ -32,7 +77,7 @@ let PicotileApp = props => {
                         <TileField field={tiles} />
                     </Route>
                     <Route exact path="/settings">
-                        <Settings />
+                        <Settings settings={settings} onChangeSetting={onChangeSetting} />
                     </Route>
                 </Switch>
             </div>
--- data/src/TileField.jsx
+++ data/src/TileField.jsx
@@ -5,11 +5,13 @@ const TileField = props => {
   const canvasRef = useRef(null)
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    renderTileField(canvas, props.field)
-  }, [])
+    const canvas = canvasRef.current  
+    var ctx = canvas.getContext('2d')
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    renderTileField(canvas, props.field.tiles)
+  }, [props.field])
   
-  return <canvas ref={canvasRef} width='300' height='300' {...props}/>
+  return <canvas ref={canvasRef} width='300' height='600' {...props}/>
 }
 
 export default TileField
@@ -35,17 +37,68 @@ export default TileField
 //      / 0  \  /
 //     /------\/
 let renderTileField = (canvas, field) => {
+
   for (let tile of field) {
-    let pos = tileToCanvasPos(tile)
+    let pos = tilePosToCanvasPos(tile.pos)
     let orientation = (Math.PI / 2) + ((1 - tile.pos.z) * Math.PI)
     switch (tile.type) {
       case 'control':
         renderControlTile(canvas, pos, orientation)
         break
       case 'light':
-        renderLightTile(canvas, pos, orientation, tile.color)
+        renderLightTile(canvas, pos, orientation, tile.color ?? "#fff")
         break
     }
+  }
+
+  renderTileGridLines(canvas, "#ddd")
+}
+
+let renderTileGridLines = (canvas, color) => {
+  let rad2 = rad - 5
+
+  // TODO: Figure out bounds from canvasPosToTile once implemented
+  var ctx = canvas.getContext('2d')
+  for (let x=0; x < 10; x++) {
+    // Down-left
+    let orientation = (Math.PI / 2) + ((1 - 0) * Math.PI)
+    let theta = orientation
+    let center1 = tilePosToCanvasPos({x: x, y: 0, z: 0})
+    let center2 = tilePosToCanvasPos({x: x, y: 10, z: 0})
+    
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(center1.x + Math.cos(theta) * rad, center1.y + Math.sin(theta) * rad)
+    ctx.lineTo(center2.x + Math.cos(theta) * rad, center2.y + Math.sin(theta) * rad)
+    ctx.stroke()
+  }
+
+  for (let x=-5; x < 5; x++) {
+    // Down-right
+    let orientation = (Math.PI / 2) + ((1 - 1) * Math.PI)
+    let theta = orientation
+    let center1 = tilePosToCanvasPos({x: x, y: -1, z: 1})
+    let center2 = tilePosToCanvasPos({x: x+10, y: 9, z: 1})
+    
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(center1.x + Math.cos(theta) * rad, center1.y + Math.sin(theta) * rad)
+    ctx.lineTo(center2.x + Math.cos(theta) * rad, center2.y + Math.sin(theta) * rad)
+    ctx.stroke()
+  }
+
+  for (let y=0; y < 10; y++) {
+    // Horizontal
+    let orientation = (Math.PI / 2) + ((1 - 0) * Math.PI)
+    let theta = orientation
+    let center1 = tilePosToCanvasPos({x: 0, y: y, z: 0})
+    let center2 = tilePosToCanvasPos({x: 10, y: y, z: 0})
+    
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(center1.x + Math.cos(theta) * rad, center1.y + Math.sin(theta) * rad)
+    ctx.lineTo(center2.x + Math.cos(theta) * rad, center2.y + Math.sin(theta) * rad)
+    ctx.stroke()
   }
 }
 
@@ -94,13 +147,13 @@ let renderControlTile = (canvas, center, orientation) => {
 }
 
 // TODO: Insert more helpful geometry diagram
-let tileToCanvasPos = (tile) => {
+let tilePosToCanvasPos = (tilePos) => {
   let base = rad * Math.cos(Math.PI / 6)
   let h = rad * Math.sin(Math.PI / 6)
 
-  const x = rad + 2 * base * tile.pos.x - base * tile.pos.y + base * tile.pos.z
-  const y = rad + (rad + h) * tile.pos.y - (rad - h) * tile.pos.z
-  console.log(`${tile.pos.x},${tile.pos.y},${tile.pos.z} => ${x}, ${y}`)
+  const x = rad + 2 * base * tilePos.x - base * tilePos.y + base * tilePos.z
+  const y = rad + (rad + h) * tilePos.y - (rad - h) * tilePos.z
+  console.log(`${tilePos.x},${tilePos.y},${tilePos.z} => ${x}, ${y}`)
   return {x, y}
 }
 
