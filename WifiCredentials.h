759da14e465ebcec04515af63dc52e770894d66e
Brian Holley <brian.holley@hotmail.com>
2021-08-19 18:40:12 -0700

    Add the basic wifi, webserver, ws server implementation and settings/tiles serving

---
+++ WifiCredentials.h
@@ -0,0 +1,11 @@
+// WifiCredentials.h
+#ifndef __PICOTILE_WIFICREDENTIALS__
+#define __PICOTILE_WIFICREDENTIALS__ 1
+
+// Placeholder file to enter your wifi network credentials to allow
+// the ESP8266 to connect
+
+const char * ssid = "";
+const char * password = "";
+
+#endif // ifndef __PICOTILE_WIFICREDENTIALS__
